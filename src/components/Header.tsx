65ad40481b956f92fdb37f516886252c55b9b6ee
Abhiraj Kumar <kumarshishu99@gmail.com>
2024-02-13 23:33:21 +0530

    HomeScreen Components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,35 @@
 /* eslint-disable prettier/prettier */
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { COLORS, FONTFAMILY, FONTSIZE, SPACING } from '../theme/theme';
+import GradientBgIcon from './GradientBgIcon';
+import ProfilePic from './ProfilePic';
 
-const Header = () => {
+interface HeaderProps {
+    title? : String
+}
+const Header:React.FC<HeaderProps> = ({title}) => {
   return (
-    <View>
-      <Text>Header</Text>
+    <View style={styles.container}>
+      <GradientBgIcon name="menu" size={FONTSIZE.size_18} color={COLORS.primaryLightGreyHex} />
+      <Text style={styles.titleText}>{title}</Text>
+      <ProfilePic />
     </View>
   );
 };
 
 export default Header;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+   container: {
+        padding: SPACING.space_30,
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-between',
+   },
+   titleText:{
+    fontFamily:FONTFAMILY.poppins_regular,
+    fontSize:FONTSIZE.size_20,
+    color:COLORS.primaryWhiteHex,
+   },
+});
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,12 @@
 /* eslint-disable prettier/prettier */
-import { StyleSheet, Text, View } from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import { ScrollView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import React, { useState } from 'react';
 import { useStore } from '../store/store';
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+import { BORDERRADIUS, COLORS, FONTSIZE, SPACING } from '../theme/theme';
+import Header from '../components/Header';
+import CustomIcon from '../components/CustomIcon';
 
 const getCategoriesFromCoffeeData = (coffeeData: any) => {
   let tempCategory: any = ['All'];
@@ -24,7 +28,7 @@ const HomeScreen = () => {
   const BeanList = useStore((state: any) => state.BeanList);
 
   const [categories, setCategories] = useState(getCategoriesFromCoffeeData(CoffeeList));
-  const [searchText, setSearchText] = useState(undefined);
+  const [searchText, setSearchText] = useState('');
   const [categoryIndex, setCategoryIndex] = useState({
     index: 0,
     category: categories[0],
@@ -33,12 +37,98 @@ const HomeScreen = () => {
 
   const tabBarHeight = useBottomTabBarHeight();
   return (
-    <View>
-      {/* Header */}
+    <View style={styles.container}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.scrollViewStyle}
+       >
+      <Header />
+      <Text style={styles.bestTitleText}>{'Find the best\ncoffee for you'}</Text>
+      <View style={styles.searchContainer}>
+        <CustomIcon style={{marginRight:10}} name="search" size={20} color={COLORS.primaryLightGreyHex} />
+        <TextInput
+          value={searchText}
+          onChangeText={(text) => setSearchText(text)}
+          placeholder="Find Your Coffee..."
+          placeholderTextColor={COLORS.primaryLightGreyHex}
+          style={styles.searchText}
+         />
+      </View>
+
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.categoryContainerStyle}
+      >
+        {
+          categories.map((category: any, index: any) => (
+            <TouchableOpacity
+              onPress={() => {
+                setCategoryIndex({
+                  index: index,
+                  category: categories[index],
+                });
+                setSortedCoffee(getCoffeeList(categoryIndex.category, CoffeeList));
+              }}
+              key={index}
+              style={styles.currCategory}
+            >
+              <Text style={[styles.categoryText, categoryIndex.index === index ? {color:COLORS.primaryOrangeHex} : {color:COLORS.secondaryLightGreyHex}]}>{category}</Text>
+              {categoryIndex.index === index ? <View style={styles.activeItem} /> : <></>}
+            </TouchableOpacity>
+          ))
+        }
+      </ScrollView>
+      </ScrollView>
     </View>
   );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor:COLORS.primaryBlackHex,
+    flex:1,
+  },
+  scrollViewStyle:{
+    flexGrow:1,
+  },
+  bestTitleText: {
+    color:COLORS.primaryWhiteHex,
+    paddingHorizontal:SPACING.space_28,
+    fontSize:FONTSIZE.size_24,
+    fontWeight:'700',
+  },
+  searchContainer: {
+    flexDirection:'row',
+    alignItems:'center',
+    backgroundColor:COLORS.primaryGreyHex,
+    marginHorizontal:SPACING.space_28,
+    marginVertical:SPACING.space_24,
+    paddingHorizontal:SPACING.space_15,
+    borderRadius:BORDERRADIUS.radius_25,
+  },
+  searchText:{
+    color:COLORS.primaryWhiteHex,
+  },
+  categoryText:{
+    fontSize:FONTSIZE.size_18,
+    fontWeight:'600',
+  },
+  currCategory:{
+    paddingHorizontal:10,
+  },
+  activeItem:{
+    height:SPACING.space_10,
+    width:SPACING.space_10,
+    backgroundColor:COLORS.primaryOrangeHex,
+    borderRadius:BORDERRADIUS.radius_25,
+    alignSelf:'center',
+    marginTop:SPACING.space_8,
+  },
+  categoryContainerStyle:{
+    paddingHorizontal:SPACING.space_10,
+  },
+});
